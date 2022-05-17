204f81e7150b3292ba5f971a43a901bcc4769ba0
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2022-05-17 11:42:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const hiraganasLocalStorageKeyword = "hiraganas";
+
+let currentHiragana = {};
+
+// ELEMENTS
+const answerInputEl = document.querySelector(".hrContainer__answerInput");
+const hiraganaContainerEl = document.getElementsByClassName(
+  "hrContainer__hiragana"
+)[0];
+
+function getOldHiraganas() {
+  let oldHiraganas = localStorage.getItem(hiraganasLocalStorageKeyword);
+  return oldHiraganas === null ? null : JSON.parse(oldHiraganas);
+}
+
+function setOldHiraganas(newOldHiraganas = []) {
+  return localStorage.setItem(
+    hiraganasLocalStorageKeyword,
+    JSON.stringify(newOldHiraganas)
+  );
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomHiragana() {
+  return hiraganas[getRandomInt(hiraganas.length)];
+}
+
+function setRandomHiraganaToHTML() {
+  const hiragana = getRandomHiragana();
+
+  let oldHiraganas = getOldHiraganas();
+  if (oldHiraganas === null) {
+    oldHiraganas = [];
+    setOldHiraganas(oldHiraganas);
+  } else {
+    for (let i = 0; i < oldHiraganas.length; i++) {
+      if (oldHiraganas[i].character === hiragana.character) {
+        return setRandomHiraganaToHTML();
+      }
+    }
+    if (oldHiraganas.length >= 50) {
+      oldHiraganas.shift();
+    }
+
+    oldHiraganas.push(hiragana);
+    setOldHiraganas(oldHiraganas);
+  }
+  currentHiragana = hiragana;
+  hiraganaContainerEl.innerHTML = hiragana["character"];
+}
+setRandomHiraganaToHTML();
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+function onSubmit() {
+  const answer = answerInputEl.value;
+  if (answer === currentHiragana["romanization"]) {
+    hiraganaContainerEl.classList.add("success");
+
+    setTimeout(() => {
+      hiraganaContainerEl.classList.remove("success");
+      setRandomHiraganaToHTML();
+      answerInputEl.value = "";
+    }, 500);
+  } else {
+    hiraganaContainerEl.classList.add("tryAgain");
+
+    setTimeout(() => {
+      hiraganaContainerEl.classList.remove("tryAgain");
+    }, 500);
+  }
+}
